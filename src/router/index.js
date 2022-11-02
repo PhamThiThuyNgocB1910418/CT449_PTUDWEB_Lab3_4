cc3aa01306b473fac0cddf6f535b7f8db8f7f3a4
ThuyNgoc22 <ngocb1910418@student.ctu.edu.vn>
2022-11-02 16:56:58 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,13 @@ const routes = [
     {
     path: "/",
     name: "contactbook",
-    component: ContactBook,
-    },
+    component: ContactBook, 
+    }, 
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    }
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
